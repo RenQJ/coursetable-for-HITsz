b7015a95572376f32b5d5a12cb9d959ac4b2f5e3
Qingji <renqjhit@163.com>
2021-03-04 17:38:36 +0800

    加一些注释

--- table.py
+++ table.py
@@ -4,6 +4,9 @@ import numpy as np
 from ics import Calendar, Event
 
 course = []
+'''
+解析每一条目，从字符串中分割出有用的信息
+'''
 def sp(str, week, time):
     arrays = str.split('\n')
     for i in range(0, len(arrays), 3):
@@ -31,25 +34,30 @@ def sp(str, week, time):
                     tmp[2] = j
                     course.append(tmp)
 
-
+'''
+读表格
+'''
 df = pd.read_excel('1.xlsx')
 for index, row in df.iterrows():
     for col in range(1,8):
         if not pd.isna(row[col]):
             sp(row[col], col, index)
 
+#自定义上课时间，和开学第一天
 time = {0:[8,0,9,45],1:[10,0,11,45],2:[14,0,15,45],3:[16,0,17,45],4:[18,45,20,30],5:[20,45,22,30]}
 first_day = datetime(2021, 2, 22)
+#由周信息转换成日期
 for i in range(len(course)):
     item = course[i]
     week = item[2]-1
     day = item[4]
     clas = item[5]
-    dtstart = first_day + timedelta(days=week*7+day, hours=time[clas][0]-8, minutes=time[clas][1])
+    dtstart = first_day + timedelta(days=week*7+day, hours=time[clas][0]-8, minutes=time[clas][1])#我不知道咋改时区，就这么凑活用吧
     dtend = first_day + timedelta(days=week*7+day, hours=time[clas][2]-8, minutes=time[clas][3])
     item.append(dtstart)
     item.append(dtend)
 
+#添加每节课
 c = Calendar()
 for i in range(len(course)):
     item = course[i]
@@ -60,6 +68,7 @@ for i in range(len(course)):
     e.location = item[3]
     e.description = item[1]
     c.events.add(e)
+#在每周一标记一下第几周
 for i in range(18):
     dtstart = (first_day + timedelta(days=7*i, hours=-8)).strftime("%Y-%m-%d %H:%M:%S")
     dtend = (first_day + timedelta(days=7*i, hours=-7)).strftime("%Y-%m-%d %H:%M:%S")
@@ -69,6 +78,5 @@ for i in range(18):
     e.begin = dtstart
     e.end = dtend
     c.events.add(e)
-print(c.events)
 with open('course_table.ics', 'w') as my_file:
     my_file.writelines(c)
